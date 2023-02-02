46d311e04865788e6e7eae245901cfaf5633dce5
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 11:55:46 +0100

    feat: :sparkles: Head component

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends HTMLAttributes<HTMLHeadElement> {}
+</script>
+
+<head {...$$restProps}>
+	<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+	<slot />
+</head>
