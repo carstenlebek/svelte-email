d7f1c1f63a2bf0305e7a971a4e84c53f2b512224
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:18:42 +0100

    feat: :sparkles: Img component

---
+++ src/lib/components/Img.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLImageElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+		alt: string;
+		src: string;
+		width: number;
+		height: number;
+	}
+
+	export let style = {};
+	export let alt = '';
+	export let src = '';
+	export let width = 0;
+	export let height = 0;
+
+	const styleDefault = {
+		display: 'block',
+		outline: 'none',
+		border: 'none',
+		textDecoration: 'none',
+		...style
+	};
+</script>
+
+<img {alt} {src} {width} {height} style={styleToString(styleDefault)} {...$$restProps} />
