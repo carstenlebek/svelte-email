14e7d0e564072cd2d8b6e6ab23b9606476403ff1
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 11:10:25 +0100

    first commit

---
+++ src/lib/components/Body.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let style: Record<string, string | number> = {};
+	let styleString = style
+		? Object.entries(style)
+				.map(([key, value]) => `${key}: ${value}`)
+				.join(';')
+		: '';
+</script>
+
+<body {...$$restProps} style={styleString}>
+	<slot />
+</body>
---
+++ src/lib/utils.ts
@@ -0,0 +1,64 @@
+export const copyTextToClipboard = async (text: string) => {
+	try {
+		await navigator.clipboard.writeText(text);
+	} catch {
+		throw new Error('Not able to copy');
+	}
+};
+
+export const pxToPt = (px: string): number | null =>
+	isNaN(Number(px)) ? null : (parseInt(px, 10) * 3) / 4;
+
+export interface Margin {
+	m?: string;
+	mx?: string;
+	my?: string;
+	mt?: string;
+	mr?: string;
+	mb?: string;
+	ml?: string;
+}
+
+export const withMargin = (props: Margin) =>
+	[
+		withSpace(props.m, ['margin']),
+		withSpace(props.mx, ['marginLeft', 'marginRight']),
+		withSpace(props.my, ['marginTop', 'marginBottom']),
+		withSpace(props.mt, ['marginTop']),
+		withSpace(props.mr, ['marginRight']),
+		withSpace(props.mb, ['marginBottom']),
+		withSpace(props.ml, ['marginLeft'])
+	].filter((s) => Object.keys(s).length)[0];
+
+const withSpace = (value: string | undefined, properties: string[]) => {
+	return properties.reduce((styles, property) => {
+		if (value) {
+			return { ...styles, [property]: `${value}px` };
+		}
+		return styles;
+	}, {});
+};
+
+// https://stackoverflow.com/a/61410824
+
+export const styleToString = (style: Record<string, string | number>) => {
+	return Object.keys(style).reduce(
+		(acc, key) =>
+			acc +
+			key
+				.split(/(?=[A-Z])/)
+				.join('-')
+				.toLowerCase() +
+			':' +
+			style[key] +
+			';',
+		''
+	);
+};
+
+export const unreachable = (
+	condition: never,
+	message = `Entered unreachable code. Received '${condition}'.`
+): never => {
+	throw new TypeError(message);
+};
