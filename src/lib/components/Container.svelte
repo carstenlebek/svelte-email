b872a2d207de7335864fe8b75e6ac2fec4e2c43a
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 11:52:49 +0100

    feat: :sparkles: Container component

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLDivElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+	}
+
+	export let style = {};
+
+	const styles = { maxWidth: '37.5em', ...style };
+	const inlineStyle = styleToString(styles);
+</script>
+
+<div>
+	{@html `<!--[if mso | IE]>
+          <table role="presentation" width="100%" align="center" style="${inlineStyle}"><tr><td></td><td style="width:37.5em;background:#ffffff">
+        <![endif]-->`}
+</div>
+<div {...$$restProps} style={inlineStyle}>
+	<slot />
+</div>
+<div>
+	{@html `<!--[if mso | IE]>
+          </td><td></td></tr></table>
+          <![endif]-->`}
+</div>
