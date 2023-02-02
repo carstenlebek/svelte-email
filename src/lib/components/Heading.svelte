b6c104b65f38be2b078090b7fc1462afe355d3f1
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:08:50 +0100

    feat: :sparkles: Heading component

---
+++ src/lib/components/Heading.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { styleToString, withMargin } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLHeadingElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+		as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+		m?: string;
+		mx?: string;
+		my?: string;
+		mt?: string;
+		mr?: string;
+		mb?: string;
+		ml?: string;
+	}
+
+	export let style = {};
+	export let as = 'h1';
+</script>
+
+<svelte:element
+	this={as}
+	style={styleToString({
+		...withMargin({
+			m: $$props.m,
+			mx: $$props.mx,
+			my: $$props.my,
+			mt: $$props.mt,
+			mr: $$props.mr,
+			mb: $$props.mb,
+			ml: $$props.ml
+		}),
+		...style
+	})}
+	{...$$restProps}
+>
+	<slot />
+</svelte:element>
