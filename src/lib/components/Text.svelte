5b770718bac9c30b07efbb5d0ced8dca42e26133
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:48:06 +0100

    feat: :sparkles: Text component

---
+++ src/lib/components/Text.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLParagraphElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+	}
+
+	export let style: $$Props['style'] = {};
+
+	const styleDefault = {
+		fontSize: '14px',
+		lineHeight: '24px',
+		margin: '16px 0',
+		...style
+	};
+</script>
+
+<p style={styleToString(styleDefault)} {...$$restProps}>
+	<slot />
+</p>
