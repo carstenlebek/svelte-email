538354c3da1118a1e7bdd925110aeb4dc717f1fd
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 11:46:44 +0100

    feat: :sparkles: Column Component

---
+++ src/lib/components/Column.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLDivElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+	}
+
+	export let style: $$Props['style'] = {};
+
+	const styleDefault = {
+		display: 'inline-flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		...style
+	};
+</script>
+
+<td style={styleToString(styleDefault)} role="presentation" {...$$restProps}>
+	<slot />
+</td>
