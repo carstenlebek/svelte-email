c5439ea2d36eeedd038e4367d37ca80b82da2e81
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:21:10 +0100

    feat: :sparkles: Link component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLAnchorElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+		target?: string;
+		href: string;
+	}
+
+	export let style = {};
+	export let target = '_blank';
+	export let href = '';
+
+	const styleDefault = {
+		color: '#067df7',
+		textDecoration: 'none',
+		...style
+	};
+</script>
+
+<a {...$$restProps} {href} {target} style={styleToString(styleDefault)}>
+	<slot />
+</a>
