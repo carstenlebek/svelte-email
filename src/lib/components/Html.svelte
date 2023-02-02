d41373027c118ce53ee6f3755ea89bfd6681ca5e
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:13:50 +0100

    feat: :sparkles: Html component

---
+++ src/lib/components/Html.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLHtmlElement>, 'style'> {
+		lang?: string;
+	}
+
+	export let lang = 'en';
+</script>
+
+<html {...$$restProps} id="__svelte-email" {lang}>
+	<slot />
+</html>
