3ef7b10b7558f0f06660230fbfee635c52142c4b
carstenlebek <carsten.lebek@googlemail.com>
2023-02-02 12:11:45 +0100

    feat: :sparkles: Hr component

---
+++ src/lib/components/Hr.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLHRElement>, 'style'> {
+		style?: Record<string, string | number | null>;
+	}
+
+	export let style: $$Props['style'] = {};
+
+	const styleDefault = {
+		width: '100%',
+		border: 'none',
+		borderTop: '1px solid #eaeaea',
+		...style
+	};
+</script>
+
+<hr style={styleToString(styleDefault)} {...$$restProps} />
