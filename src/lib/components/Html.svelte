98417222c9e9cd15a6ef2feb25fd266e38154d0c
Steven Polak <steven-21-3@hotmail.com>
2023-09-27 15:34:27 +0100

    install jsdom to manipulate the rendered html server-side; working script to detect tw usage and render tw classes; skip frontend visualization for now

--- src/lib/components/Html.svelte
+++ src/lib/components/Html.svelte
@@ -2,11 +2,14 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	interface $$Props extends Omit<HTMLAttributes<HTMLHtmlElement>, 'style'> {
 		lang?: string;
+		useTailwind?: "yes" | "no"
 	}
 
 	export let lang = 'en';
+	export let useTailwind = "yes"
+
 </script>
 
-<html {...$$restProps} id="__svelte-email" {lang}>
+<html {...$$restProps} id="__svelte-email" {lang} data-tw={useTailwind}>
 	<slot />
 </html>
